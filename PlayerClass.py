fcec369db19673e73cd24181f860bd2307b3c05a
Biljana <61354731+biljanazecic@users.noreply.github.com>
2022-05-04 16:10:48 +0200

    Update PlayerClass.py

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,1 +1,21 @@
 
+class Player:
+  
+  def __init__(self, name):
+    self.name = name
+    self.all_cards = []
+    
+  def remove_one(self):
+    return self.all_cards.pop(0)
+  
+  def add_cards(self, new_cards):
+    if type(new_cards) == type([]):
+      #List of multiple Card objects
+      self.all_cards.extend(new_cards)
+     else:
+      #For a single card object
+      self.all_cards.append(new_cards)
+  
+  def __str__(self):
+    return f'Player {self.name} has {len(self.all_cards)} cards.'
+    
